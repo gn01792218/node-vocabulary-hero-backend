af933cd75ff7682e7dc402a97caedd9d12815be6
MSI\gn01792218 <gn01792218@hotmail.com>
2024-05-23 11:48:36 +0800

    [feature] generate accessToken when login success

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,73 +1,107 @@
-import { User } from '@prisma/client'
-import UserRepository from '../repositories/userRepository'
-import { Request, Response } from 'express'
-import { UserCreateRequest, UserLoginRequest, UserUpdateRequest } from '../types/user'
-import bcrypt from 'bcryptjs'
-import { ErrorRespons } from '../types/error'
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { User } from "@prisma/client";
+import UserRepository from "../repositories/userRepository";
+import { Request, Response } from "express";
+import {
+  UserCreateRequest,
+  UserLoginRequest,
+  UserModel,
+  UserRespons,
+  UserUpdateRequest,
+} from "../types/user";
+import { ErrorRespons } from "../types/error";
 
-const userRepo = new UserRepository()
+const userRepo = new UserRepository();
+const authSecret = process.env.AUTH_SECRET;
+const tokenExpiresDay = 3;
 
-export const getAllUser = async (req:Request,res:Response<User[]>)=>{
-    const users = await userRepo.getAll()
-    console.log(users)
-    res.status(200).json(users)
-}
-export const getUser = async (req:Request<{id:string}>,res:Response<User | ErrorRespons>)=>{
-    const user = await userRepo.getById(Number(req.params.id))
-    console.log(user)
-    if(!user) return res.status(400).json({message:"查無此User"})
-    res.status(200).json(user)
-}
-export const createUser = async (req:Request<never, never, UserCreateRequest>,res:Response<User | ErrorRespons>)=>{
-    const payload = req.body
-    if(await isThisEmailExist(payload.email)) return res.status(400).send({message:"此email已經存在!"}) //1.檢查email有沒有重複
-    if(payload.confirmPassword !== payload.password) return res.status(400).send({message:"密碼與確認密碼不同!"}) //2.檢查確認密碼
-    const user = await userRepo.add({
-        name:payload.name,
-        email:payload.email,
-        password:await bcryptPassword(payload.password),
-        provider:payload.provider
-    })
-    console.log(user)
-    res.status(200).json(user)
-}
-export const updateUser = async (req:Request<{id:string}, never, UserUpdateRequest>,res:Response<User | ErrorRespons>)=>{
-    const payload = req.body
-    if(! await isThisUserExist(Number(req.params.id))) return res.status(400).send({message:"找不到此user"})
-    payload.password = await bcryptPassword(payload.password) //加密密碼
-    const user = await userRepo.update(Number(req.params.id) ,payload)
-    console.log(user)
-    res.status(200).json(user)
-}
-export const deleteUser = async (req:Request,res:Response<User | ErrorRespons>)=>{
-    const { id } = req.params
-    if(! await isThisUserExist(Number(id))) return res.status(400).send({message:'沒有此User'})
-    const user = await userRepo.delete(Number(id))
-    console.log(user)
-    res.status(200).json(user)
-}
-export const login = async (req:Request<never, never, UserLoginRequest>, res:Response<string | ErrorRespons>)=>{
-    const payload = req.body
-    const user = await userRepo.getByEmail(payload.email)
-    if(!user) return res.status(400).send({message:'找不到該使用者'})
-    if(!await passwordCompare(payload.password, user.password)) return res.status(400).send({message:"密碼不正確!"})
-    return res.status(200).send("<h1>登入成功</h1>") 
+export const getAllUser = async (req: Request, res: Response<UserRespons[]>) => {
+  const users = await userRepo.getAll();
+  const usersDTO = users.map(user=>getUserDTO(user)) 
+  console.log(usersDTO);
+  res.status(200).json(usersDTO);
+};
+export const getUser = async ( req: Request<{ id: string }>, res: Response<UserRespons | ErrorRespons>) => {
+  const user = await userRepo.getById(Number(req.params.id));
+  if (!user) return res.status(400).json({ message: "查無此User" });
+  const userDTO = getUserDTO(user);
+  console.log(userDTO);
+  res.status(200).json(userDTO);
+};
+export const createUser = async ( req: Request<never, never, UserCreateRequest>, res: Response<UserRespons | ErrorRespons>) => {
+  const payload = req.body;
+  if (await isThisEmailExist(payload.email)) return res.status(400).send({ message: "此email已經存在!" }); //1.檢查email有沒有重複
+  if (payload.confirmPassword !== payload.password) return res.status(400).send({ message: "密碼與確認密碼不同!" }); //2.檢查確認密碼
+  const user = await userRepo.add({
+    name: payload.name,
+    email: payload.email,
+    password: await bcryptPassword(payload.password),
+    provider: payload.provider,
+  });
+  const userDTO = getUserDTO(user);
+  console.log(userDTO);
+  res.status(200).json(userDTO);
+};
+export const updateUser = async ( req: Request<{ id: string }, never, UserUpdateRequest>, res: Response<UserRespons | ErrorRespons>) => {
+  const payload = req.body;
+  if (!(await isThisUserExist(Number(req.params.id)))) return res.status(400).send({ message: "找不到此user" });
+  payload.password = await bcryptPassword(payload.password); //加密密碼
+  const user = await userRepo.update(Number(req.params.id), payload);
+  const userDTO = getUserDTO(user);
+  console.log(userDTO);
+  res.status(200).json(userDTO);
+};
+export const deleteUser = async ( req: Request, res: Response<UserRespons | ErrorRespons>) => {
+  const { id } = req.params;
+  if (!(await isThisUserExist(Number(id)))) return res.status(400).send({ message: "沒有此User" });
+  const user = await userRepo.delete(Number(id));
+  const userDTO = getUserDTO(user);
+  console.log(userDTO);
+  res.status(200).json(userDTO);
+};
+export const login = async ( req: Request<never, never, UserLoginRequest>, res: Response<UserRespons | ErrorRespons>) => {
+  const payload = req.body;
+  const user = await userRepo.getByEmail(payload.email);
+  if (!user) return res.status(400).send({ message: "找不到該使用者" });
+  if (!(await passwordCompare(payload.password, user.password))) return res.status(400).send({ message: "密碼不正確!" });
+  const userDTO = getUserDTO(user);
+  const token = createUserToken(userDTO);
+  if (token) userDTO.accessToken = token;
+  return res.status(200).send(userDTO);
+};
+
+function getUserDTO(user: User): UserRespons {
+  return {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    provider: user.provider,
+  };
 }
 
 //內部方法
-async function isThisEmailExist(email:string){
-    const user = await userRepo.getByEmail(email)
-    if(!user) return false
-    return true
+async function isThisEmailExist(email: string) {
+  const user = await userRepo.getByEmail(email);
+  if (!user) return false;
+  return true;
 }
-async function isThisUserExist(id:number){
-    const user = await userRepo.getById(id)
-    if(!user) return false
-    return true
+async function isThisUserExist(id: number) {
+  const user = await userRepo.getById(id);
+  if (!user) return false;
+  return true;
 }
-async function bcryptPassword(password:string){
-    return await bcrypt.hash(password, 12)
+async function bcryptPassword(password: string) {
+  return await bcrypt.hash(password, 12);
 }
-async function passwordCompare(inputPassword:string, comparePassword:string){
-    return await bcrypt.compare(inputPassword, comparePassword)
+async function passwordCompare(inputPassword: string, comparePassword: string) {
+  return await bcrypt.compare(inputPassword, comparePassword);
+}
+function createUserToken(user: UserRespons) {
+  if (!authSecret) return console.log("請定義好auth secret 環境變數");
+  const token = jwt.sign(user, authSecret, {
+    algorithm: "HS256",
+    expiresIn: 86400 * tokenExpiresDay, //86400代表24小時
+  });
+  return token;
 }
--- types/user.ts
+++ types/user.ts
@@ -2,11 +2,19 @@ export interface UserModel{
     id:number
     name:string,
     email:string
-    password:string,
     provider:string,  
+    accessToken?:string
 }
 
 //請求與回應
+
+export interface UserRespons{
+    id:number,
+    name:string,
+    email:string
+    provider?:string,  //沒有填DB會自動標示為email
+    accessToken?:string
+}
 export interface UserCreateRequest{
     name:string,
     email:string
@@ -35,7 +43,7 @@ export interface UserLoginRequest{
     password:string,
 }
 
-//資料庫所需的型態
+//DTOs
 export interface UserCreateDTO{
     name:string,
     email:string
